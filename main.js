99caa4e72d6fb09dc12c6e8c643417bc5d29cd25
bosro <misterbosro@gmail.com>
2022-11-09 16:55:13 +0000

    dashboard

---
+++ main.js
@@ -0,0 +1,59 @@
+const chart = document.getElementById('chart').getContext('2d');
+
+//create a new chart instance
+new Chart(chart, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov'],
+        
+     datasets: [
+        {
+         label: 'BTC',
+         data: [29374, 33577, 49631, 59095, 57828, 36684, 33572, 39984, 48847, 48116, 61004],
+         borderColor: 'red',
+         borderWidth: 2
+     },     
+     {
+     label: 'ETH',
+     data: [31500, 41000, 88800, 26000, 4600, 36698, 5000, 6000, 18656, 24832, 86844],
+     borderColor: 'blue',
+     borderWidth: 2
+    }
+  ]
+ },
+ options: {
+      
+  reponsive: true
+  }
+})
+
+
+
+
+
+//show or hidee sidebar
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sideBar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () =>{
+sideBar.style.display ='block';
+})
+
+closeBtn.addEventListener('click', () =>{
+sideBar.style.display = 'none';
+})
+
+
+
+// change theme
+
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+     
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+    
+})
